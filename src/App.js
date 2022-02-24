aa1af6a809be32dc5d2ea4fc89d5255f1a02b80f
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-02-24 14:20:33 -0500

    Implements route based code-splitting using HOC async component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import "./App.css";
 import React from "react";
 import Page1 from "./components/page1.component";
-import Page2 from "./components/page2.component";
-import Page3 from "./components/page3.component";
+// import Page2 from "./components/page2.component";
+// import Page3 from "./components/page3.component";
+import asyncComponent from "./components/async.component";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       route: "page1",
+      component: null,
     };
   }
 
   onRouteChange = (route) => {
+    // No code splitting
     this.setState({ route: route });
+    // With code splitting
+    //   if (route === "page1") {
+    //     this.setState({ route: route });
+    //   } else if (route === "page2") {
+    //     import("./components/page2.component").then((Page2) => {
+    //       this.setState({ route: route, component: Page2.default });
+    //     });
+    //   } else if (route === "page3") {
+    //     import("./components/page3.component").then((Page3) => {
+    //       this.setState({ route: route, component: Page3.default });
+    //     });
+    //   }
   };
 
   render() {
     if (this.state.route === "page1") {
       return <Page1 onRouteChange={this.onRouteChange} />;
     } else if (this.state.route === "page2") {
-      return <Page2 onRouteChange={this.onRouteChange} />;
+      const AsyncPage2 = asyncComponent(() =>
+        import("./components/page2.component")
+      );
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
     } else if (this.state.route === "page3") {
-      return <Page3 onRouteChange={this.onRouteChange} />;
+      const AsyncPage3 = asyncComponent(() =>
+        import("./components/page3.component")
+      );
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
     }
+    // if (this.state.route === "page1") {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // } else {
+    //   return <this.state.component onRouteChange={this.onRouteChange} />;
+    // }
   }
 }
 
